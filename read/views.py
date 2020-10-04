b5f51f59860cedc7e83622e9d442406a1019d958
Shona <lowdensh@gmail.com>
2020-10-04 18:24:31 +0100

    basic wip read functionality with translations

--- read/views.py
+++ read/views.py
@@ -1,5 +1,7 @@
 from .models import Author, Book, TranslatableWord
 from django.shortcuts import get_object_or_404, render
+from googletrans import Translator
+import re
 
 
 def view_books(request):
@@ -25,18 +27,52 @@ def read_wip(request):
     # change this in urls.py too
     book_id = 1
 
-    file_path = r"C:\Users\Shona\Desktop\Books\002-GINGER-THE-GIRAFFE-Free-Childrens-Book-By-Monkey-Pen-unlocked.txt"
-    # https://docs.python.org/3/library/functions.html#open
-    file = open(file_path, encoding='utf8', errors='ignore')
-    paragraph_list = file.read().split('\n\n')
+    # TODO where should translations be done and stored?
+    # user account section when selecting learning language? translations fetched on language selection?
+    # TODO translation for plurals
+    # plurals are not stored in the database - yet!
+    learning_language = 'sv'
+    english_word_list = TranslatableWord.objects.all()
+    english_word_list = [word.english_word for word in english_word_list]
+    translator = Translator()
+    foreign_word_list = translator.translate(
+        english_word_list,
+        src='en',
+        dest=learning_language)
+
+    # TODO how and where should book text be stored?
+    file_path = 'static/books/monkeypen/002-GINGER-THE-GIRAFFE-Free-Childrens-Book-By-Monkey-Pen-unlocked.txt'
+    file_text = open(file_path, encoding='utf8', errors='ignore').read()
+
+    # TODO efficiency of search/replace
+    # go through every word in word_list? or file_text?
+    # time complexity of search/replace
+    for i, english_word in enumerate(english_word_list):
+        foreign_word = foreign_word_list[i].text
+        pattern = re.compile(rf'(?<![^\W]){english_word}(?![^\W])', re.I)
+        replacement = f'<button class="tappyWord"> {foreign_word} <span> {english_word} </span></button>'
+        file_text = re.sub(pattern, replacement, file_text)
+
+    # TODO should text be large paragraphs or short sentences?
+    # text file into paragraphs
+    paragraph_list = file_text.split('\n\n')
     paragraph_list = [paragraph.replace('\n', ' ') for paragraph in paragraph_list]
+
+    # text file into paragraphs into sentences - rudimentary splitting
+    # paragraph_list = file_text.split('\n\n')
+    # paragraph_list = [paragraph.replace('\n', ' ') for paragraph in paragraph_list]
+    # sentence_list = []
+    # for paragraph in paragraph_list:
+    #     sentences = paragraph.split('. ')
+    #     for s in sentences:
+    #         sentence_list.append(s + '.')
 
     template = 'read/read-wip.html'
     book = get_object_or_404(Book, pk=book_id)
     context = {
         'book': book,
         'author_name': book.author.western_order(),
-        'word_list': TranslatableWord.objects.all(),
+        'word_list': english_word_list,
         'paragraph_list': paragraph_list
     }
     return render(request, template, context)
