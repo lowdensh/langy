4d9ef9898a4668079c2d4907a573f42f8ddd6724
Shona <lowdensh@gmail.com>
2021-05-02 08:38:42 +0100

    rename unit tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -4,19 +4,19 @@ from users.models import CustomUser
 
 
 class SignUpViewTest(SimpleTestCase):
-    def test_url_path(self):
+    def test_sign_up_url_path(self):
         response = self.client.get('/users/sign-up')
         self.assertEqual(response.status_code, 200)
 
-    def test_url_name(self):
+    def test_sign_up_url_name(self):
         response = self.client.get(reverse('users:sign_up'))
         self.assertEqual(response.status_code, 200)
 
-    def test_uses_template(self):
+    def test_sign_up_uses_template(self):
         response = self.client.get(reverse('users:sign_up'))
         self.assertTemplateUsed(response, 'users/sign-up.html')
 
-    def test_in_context(self):
+    def test_sign_up_context(self):
         response = self.client.get(reverse('users:sign_up'))
         self.assertIn('form', response.context)
 
@@ -52,26 +52,26 @@ class ProfileViewTest(TestCase):
     def setUp(self):
         login = self.client.login(email='superuser@email.com', password='pass')
 
-    def test_url_path_user_id_missing(self):
+    def test_profile_url_path_user_id_missing(self):
         response = self.client.get('/users/profile')
         self.assertEqual(response.status_code, 404)
     
-    def test_url_path(self):
+    def test_profile_url_path(self):
         uid = CustomUser.objects.get(email='superuser@email.com').id
         response = self.client.get(f'/users/profile/{uid}')
         self.assertEqual(response.status_code, 200)
 
-    def test_url_name(self):
+    def test_profile_url_name(self):
         uid = CustomUser.objects.get(email='superuser@email.com').id
         response = self.client.get(reverse('users:profile', args=(uid,)))
         self.assertEqual(response.status_code, 200)
 
-    def test_uses_template(self):
+    def test_profile_uses_template(self):
         uid = CustomUser.objects.get(email='superuser@email.com').id
         response = self.client.get(reverse('users:profile', args=(uid,)))
         self.assertTemplateUsed(response, 'users/profile.html')
 
-    def test_in_context(self):
+    def test_profile_context(self):
         uid = CustomUser.objects.get(email='superuser@email.com').id
         response = self.client.get(reverse('users:profile', args=(uid,)))
         self.assertIn('profile_user', response.context)
